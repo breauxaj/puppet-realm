4e09da98f0da919eb3ac455d98d72d9182ec3425
Adam Breaux <breauxaj@gmail.com>
2015-06-08 16:31:18 -0400

    try it

--- lib/facter/realm.rb
+++ lib/facter/realm.rb
@@ -1,5 +1,14 @@
+require 'facter'
 Facter.add(:realm) do
   setcode do
-    'desktop'
+    therealm = 'realm not set'
+    if File.exists?('/etc/realm')
+      File.open('/etc/realm', "r") do |f|
+        f.each_line do |line|
+          therealm = line.chomp.downcase
+        end
+      end
+    end
+    therealm
   end
 end
