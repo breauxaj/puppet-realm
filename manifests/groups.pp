583b59fd22fe5b5f39446e5acc545dde04c0b1cc
Adam Breaux <breauxaj@gmail.com>
2015-06-09 15:58:52 -0400

    typo

--- manifests/groups.pp
+++ manifests/groups.pp
@@ -2,6 +2,6 @@ class realm::groups {
   include ::users
   
   $realm_groups = hiera('realm::groups',{})
-  create_resources('users::add_group',$realm_group)
+  create_resources('users::add_group',$realm_groups)
 
 }
