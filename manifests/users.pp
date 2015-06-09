ca0e248e46fec46dcd8ae432b71558f9da859ac8
Adam Breaux <breauxaj@gmail.com>
2015-06-09 15:47:54 -0400

    trying to figure it out

--- manifests/users.pp
+++ manifests/users.pp
@@ -2,6 +2,6 @@ class realm::users {
   include ::users
   
   $realm_users = hiera('realm::users',{})
-  create_resources('realm::users',$realm_users)
+  create_resources('users::add_user',$realm_users)
 
 }
