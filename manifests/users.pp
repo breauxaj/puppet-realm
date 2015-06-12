8a51d85ed4a3106fbb37f3357f82404874a03fec
Adam Breaux <breauxa@email.chop.edu>
2015-06-12 09:35:02 -0400

    handle both in one wrapper

--- manifests/users.pp
+++ manifests/users.pp
@@ -4,4 +4,7 @@ class realm::users {
   $realm_users = hiera('realm::users',{})
   create_resources('users::add_user',$realm_users)
 
+  $realm_ldap_users = hiera('realm::ldap_users',{})
+  create_resources('users::add_ldap_user',$realm_ldap_users)
+
 }
